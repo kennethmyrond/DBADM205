18d860a20a90b258c373b42724b3a5eda675711c
kennethmyrond <kenneth_uy@dlsu.edu.ph>
2024-07-21 15:13:26 +0800

    updated view sales rep and main

--- employees.java
+++ employees.java
@@ -140,6 +140,9 @@ public class employees {
 
         System.out.println("Enter Employee ID:");
         employeeID = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Enter Range: \n [0] - Current \n [1] - Previous \n [2] - All Assignments");
+        int range = Integer.parseInt(sc.nextLine());
         
         Connection conn = null;
         PreparedStatement pstmt = null;
@@ -150,23 +153,39 @@ public class employees {
             System.out.println("Connection Successful");
             conn.setAutoCommit(false);
 
-            String sql = "SELECT \r\n"
-            		+ "    sra.officeCode, \r\n"
-            		+ "    sra.startDate, \r\n"
-            		+ "    sra.endDate, \r\n"
-            		+ "    sra.reason, \r\n"
-            		+ "    sra.quota, \r\n"
-            		+ "    sra.salesManagerNumber,\r\n"
-            		+ "    e.firstName AS salesManagerFirstName,\r\n"
-            		+ "    e.lastName AS salesManagerLastName\r\n"
-            		+ "FROM \r\n"
-            		+ "    salesRepAssignments sra  \r\n"
-            		+ "LEFT JOIN \r\n"
-            		+ "    employees e \r\n"
-            		+ "ON \r\n"
-            		+ "    e.employeeNumber = sra.salesManagerNumber\r\n"
-            		+ "WHERE \r\n"
-            		+ "    sra.employeeNumber = ? LOCK IN SHARE MODE";
+            String baseSql = "SELECT \r\n" + 
+                    "    sra.officeCode, \r\n" + 
+                    "    sra.startDate, \r\n" + 
+                    "    sra.endDate, \r\n" + 
+                    "    sra.reason, \r\n" + 
+                    "    sra.quota, \r\n" + 
+                    "    sra.salesManagerNumber,\r\n" + 
+                    "    e.firstName AS salesManagerFirstName,\r\n" + 
+                    "    e.lastName AS salesManagerLastName\r\n" + 
+                    "FROM \r\n" + 
+                    "    salesRepAssignments sra\r\n" + 
+                    "LEFT JOIN \r\n" + 
+                    "    employees e \r\n" + 
+                    "ON \r\n" + 
+                    "    e.employeeNumber = sra.salesManagerNumber\r\n" + 
+                    "WHERE \r\n" + 
+                    "    sra.employeeNumber = ?\r\n";
+
+            String condition = "";
+            switch (range) {
+                case 0:
+                    condition = "AND (sra.endDate IS NULL OR sra.endDate > CURDATE()) AND sra.startDate \r\n";
+                    break;
+                case 1:
+                    condition = "AND sra.endDate <= CURDATE() \r\n";
+                    break;
+                case 2:
+                    // No additional condition needed for case 2
+                    break;
+            }
+        
+            String sql = baseSql + condition + "LOCK IN SHARE MODE;";
+
             pstmt = conn.prepareStatement(sql);
             pstmt.setInt(1, employeeID);
 
@@ -174,7 +193,6 @@ public class employees {
             sc.nextLine();
 
             rs = pstmt.executeQuery();
-            
             boolean assignmentsFound = false;
             while (rs.next()) {
             	assignmentsFound = true;
@@ -197,7 +215,7 @@ public class employees {
             }
 
             if (!assignmentsFound) {
-                System.out.println("No sales rep assignments found for employee ID: " + employeeID);
+                System.out.println("No record found for employee ID: " + employeeID);
             }
 
             rs.close();
@@ -226,24 +244,59 @@ public class employees {
     }
 
     public static void main(String[] args) {
-        Scanner sc     = new Scanner (System.in);
-        int     choice;
-        // Letting the user choose between the functions
-        // kenneth 0, 2, 4 
-        // laiven 1, 3, 5
-        System.out.println("Enter Type: \n  [0] - CREATE AN EMPLOYEE \n  [1] - RECLASSIFY EMPLOYEE \n  [2] - RESIGN EMPLOYEE \n  [3] - CREATE NEW SALES REP ASSIGNMENT OR SALES REP ONLY \n  [4] - VIEW ALL DETAILS OF SALES REP (PREVIOUS ASSIGN) \n  [5] - VIEW BASIC EMPLOYEE RECORD ");
+        Scanner sc = new Scanner(System.in);
+        int choice;
 
-        choice = sc.nextInt();
         employees e = new employees();
-        if (choice==0) e.addEmployee();
-        if (choice==1) e.reclassifyEmployee();
-        if (choice==2) e.resignEmployee();
-        if (choice==3) e.createSalesRepAssign();
-        if (choice==4) e.viewSalesRepDetails();
-        if (choice==5) e.viewEmployee();
-        
-        System.out.println("Press enter key to continue....");
-        sc.nextLine();
+
+        do {
+            // Displaying the menu options to the user
+            System.out.println("Enter Type: \n" +
+                    "  [0] - CREATE AN EMPLOYEE \n" +
+                    "  [1] - RECLASSIFY EMPLOYEE \n" +
+                    "  [2] - RESIGN EMPLOYEE \n" +
+                    "  [3] - CREATE NEW SALES REP ASSIGNMENT OR SALES REP ONLY \n" +
+                    "  [4] - VIEW ALL DETAILS OF SALES REP (PREVIOUS ASSIGN) \n" +
+                    "  [5] - VIEW BASIC EMPLOYEE RECORD \n" +
+                    "  [9] - EXIT");
+
+            choice = sc.nextInt();
+            sc.nextLine(); // Consume the newline character
+
+            // Handling user choices
+            switch (choice) {
+                case 0:
+                    e.addEmployee();
+                    break;
+                case 1:
+                    e.reclassifyEmployee();
+                    break;
+                case 2:
+                    e.resignEmployee();
+                    break;
+                case 3:
+                    e.createSalesRepAssign();
+                    break;
+                case 4:
+                    e.viewSalesRepDetails();
+                    break;
+                case 5:
+                    e.viewEmployee();
+                    break;
+                case 9:
+                    System.out.println("Exiting the program...");
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again.");
+            }
+
+            if (choice != 9) {
+                System.out.println("Press enter key to continue....");
+                sc.nextLine();
+            }
+
+        } while (choice != 9);
+
         sc.close();
     }
 }
